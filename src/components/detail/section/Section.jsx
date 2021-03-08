738f73a110ccdf746ee235fc6be10773d7be42af
feelslikemmmm <67893516+feelslikemmmm@users.noreply.github.com>
2021-03-08 22:53:27 +0900

    detail backBtn update

--- src/components/detail/section/Section.jsx
+++ src/components/detail/section/Section.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import styles from '../../../styles/detail/section/Section.module.css';
 const Section = ({ item }) => {
   return (
     <div className={styles.section__container}>
+      <div className={styles.backBtn}>
+        <Link to="/" className={styles.product_name}>
+          ←
+        </Link>
+      </div>
       <h1 className={styles.title__container}>
         <span>{item.title}</span>
       </h1>
