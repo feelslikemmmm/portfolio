4a7f41345765e6b82de23dc52af53263596eb545
feelslikemmmm <67893516+feelslikemmmm@users.noreply.github.com>
2021-03-09 17:31:31 +0900

    datas update

--- src/data/Datas.js
+++ src/data/Datas.js
@@ -33,7 +33,6 @@ let Datas = [
     title: 'Portfolio Project',
     subtitle: 'Front-end Portfolio',
     desc: '프로젝트를 모아서 소개하는 웹 포트폴리오 프로젝트 입니다',
-    desc2: 'Home project Detatil',
     itemTitle: ['Assignment', 'Objective', 'Project Includes', 'Year'],
     assignment: ['HTML 마크업', 'Module css 스타일링', 'React-hook', 'React-reveal 라이브러리를 활용한 스크롤 이벤트', 'React-route 를 통한 페이지 분기 처리'],
     objective: ['Navbar Link를 통해 github와, blog로 이동할 수 있습니다', '상단 email 혹은 contact rens 클릭 시 이메일을 발송할 수 있습니다', '각 프로젝트 별 깃허브 주소와 배포 된 웹사이트 그리고 추가 설명을 볼 수 있는 페이지로 이동할 수 있습니다',],
