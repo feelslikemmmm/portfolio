98637b67fb7139a4ca6c4f707f4dd53bb89b4cd2
feelslikemmmm <67893516+feelslikemmmm@users.noreply.github.com>
2023-06-30 06:21:02 +0900

    feat: update module borderText

---
+++ src/components/ui/BorderText.tsx
@@ -0,0 +1,13 @@
+interface BorderTextProps {
+  text: string;
+}
+
+const BorderText = ({ text }: BorderTextProps) => {
+  return (
+    <p className="inline text-black whitespace-nowrap decoration-none border solid border-black2 rounded-[30em] px-[0.2em] pb-[1px] transition-all ease-in-out duration-[200ms] hover:bg-[#472A18] hover:text-[#fff]">
+      {text}
+    </p>
+  );
+};
+
+export default BorderText;
