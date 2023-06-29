ceee4241d7e18f1ac12bda0ebc81672149d9ee08
feelslikemmmm <67893516+feelslikemmmm@users.noreply.github.com>
2023-06-30 06:07:20 +0900

    feat: create footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,14 @@
+import IndexPageContent from './IndexPageContent';
+
+const Footer = () => {
+  return (
+    <IndexPageContent>
+      <div className="font-playfair pt-[100px] pb-[40px] flex justify-center items-end">
+        <span className="text-[4vh] md:text-[10vh]">@</span>
+        <h1 className="text-[6vh] md:text-[14vh]">Feelslikemmmm</h1>
+      </div>
+    </IndexPageContent>
+  );
+};
+
+export default Footer;
