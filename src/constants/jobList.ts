9d4a5ab823e428c2866950519ad52773358f081a
feelslikemmmm@gmail.com <67893516+feelslikemmmm@users.noreply.github.com>
2025-01-05 01:22:43 +0900

    chore(low): update jobList

---
+++ src/constants/jobList.ts
@@ -0,0 +1,15 @@
+export type JobType = {
+  id: number;
+  title: string;
+}[];
+
+export const JobList: JobType = [
+  {
+    id: 0,
+    title: `"글로벌 엔터테크 서비스를 운영하는 빅크에서 프론트엔드 개발자로 재직했어요"`,
+  },
+  {
+    id: 1,
+    title: `"미디어 커머스 쇼핑 앱 포크 쇼핑을 운영하는 포크홀딩스에서 프론트엔드 개발자로 재직했어요"`,
+  },
+];
