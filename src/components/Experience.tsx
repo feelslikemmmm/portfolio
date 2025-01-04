b0a6c4985cd26c43892afb3d24710f66a74cb591
feelslikemmmm@gmail.com <67893516+feelslikemmmm@users.noreply.github.com>
2025-01-05 01:23:33 +0900

    styles(low): jobs 추가

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -1,5 +1,9 @@
 import IndexPageContent from './IndexPageContent';
 import ExperienceCard from './ExperienceCard';
+import {
+  bigcEperienceList,
+  forcEperienceList,
+} from '@constants/experienceList';
 
 const Experience = () => {
   return (
@@ -19,15 +23,28 @@ const Experience = () => {
               </em>
             </h3>
           </div>
-          <div className="mt-[10px] text-[16px] leading-[1.4em] text-black">
-            <span>
-              "미디어 커머스 쇼핑 앱 포크 쇼핑을 운영하는 포크홀딩스에서
-              프론트엔드 개발자로 재직했어요"
-            </span>
-          </div>
         </div>
       </div>
-      <ExperienceCard />
+      <div className="flex flex-col gap-10 pt-4">
+        <div className="flex flex-col gap-4">
+          <div className="flex flex-col gap-4 mt-[10px] text-[18px] leading-[1.4em] text-black font-[500]">
+            <p className="font-[600] text-[24px]">BIGC</p>
+            <span>
+              "글로벌 엔터테크 서비스를 운영하는 빅크에서 프론트엔드 개발자로
+              재직했어요"
+            </span>
+          </div>
+          <ExperienceCard experienceList={bigcEperienceList} />
+        </div>
+        <div className="flex flex-col gap-4 mt-[10px] text-[18px] leading-[1.4em] text-black font-[500]">
+          <p className="font-[600] text-[24px]">Forc</p>
+          <span>
+            "미디어 커머스 쇼핑 앱 포크 쇼핑을 운영하는 포크홀딩스에서
+            프론트엔드 개발자로 재직했어요"
+          </span>
+          <ExperienceCard experienceList={forcEperienceList} />
+        </div>
+      </div>
     </IndexPageContent>
   );
 };
